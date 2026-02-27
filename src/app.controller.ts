86945d892bae47457be9ca6651bd653bc7427b2e
MEHMOOD-84 <mehmood@codek.tech>
2026-02-27 13:02:04 +0500

    create calculate function

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('calculate')
+  calculate() {
+    return this.appService.calculate();
+  }
 }
